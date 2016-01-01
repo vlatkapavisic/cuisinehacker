94465a41c06d581fa0d77cdac1820b589859a712
Debellatrix <vlatka.pavisic@gmail.com>
2016-01-01 19:54:13 +0100

    Page title etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 	def pagetitle
-		"localstove:3000 | #{content_for(:title)}".html_safe 
+		content_for?(:title) ? "lowcalhost:3000 | #{content_for(:title)}".html_safe : "lowcalhost:3000"
 	end
 end
