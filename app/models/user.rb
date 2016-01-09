7c74cf15ae07276d7fb36c5dc13521922207fc78
Debellatrix <vlatka.pavisic@gmail.com>
2016-01-09 18:10:16 +0100

    Design

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 end
