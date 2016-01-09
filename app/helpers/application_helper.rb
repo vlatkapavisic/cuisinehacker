ee058ebeda4374859321e1236df3a0e83071cef5
Debellatrix <vlatka.pavisic@gmail.com>
2016-01-09 19:58:15 +0100

    Blog name, design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 	def pagetitle
-		content_for?(:title) ? "lowcalhost:3000 | #{content_for(:title)}".html_safe : "lowcalhost:3000"
+		content_for?(:title) ? "#{blog_name} | #{content_for(:title)}".html_safe : blog_name
 	end
+
+  def blog_name
+    'Cuisine Hacker'
+  end
 end
