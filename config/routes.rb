e563d478a9823c865c9afb6cf8110916e7eb0e30
Vlatka Pavisic <vlatka.pavisic@gmail.com>
2017-12-17 12:30:49 +0100

    Update places page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about', as: 'about'
   get '/search' => 'recipes#search', as: 'search'
   get '/posts-archive' => 'posts#archive', as: 'posts_archive'
-  get '/a-moveable-feast' => 'places#map', as: 'places_map'
+  get '/bonnes-adresses-parisiennes' => 'places#map', as: 'places_map'
   get '/recipes/category/:category' => 'recipes#category_show', as: 'recipes_category'
 
   resources :recipes
